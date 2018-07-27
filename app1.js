02b609a17f9633621bffaab4f523ce64eea57505
ethornbury <ethornbury@eircom.net>
2018-07-27 20:27:19 +0000

    first commit, cloned from LiamMcC

--- app1.js
+++ app1.js
@@ -8,44 +8,31 @@ app.use(express.static("scripts")); // allow the application to access the scrip
 app.use(express.static("images")); // allow the application to access the images folder contents to use in the application
 app.use(express.static("views")); // Allow access to content of views folder
 
-//my gearhost MYSQL db credentials to create a connection
 var mysql = require('mysql');
 const db = mysql.createConnection({
-  host     : 'den1.mysql4.gear.host',
-  user     : 'emernode',
-  password : 'Jt55Wlv-2a2-',
-  database : 'emernode'
+  host     : 'den1.mssql3.gear.host',
+  user     : 'emertest',
+  password : 'Fk732EQ?XAt?',
+  database : 'emertest'
 });
 
-//Using the db above to connect to gearhost db
 db.connect((err) => {
   if(err){
     //throw err;
     console.log("db connect broke", err);
   }else{
-    console.log("Getting there liam, connected to gearhost DB...");
+    console.log("Getting there liam connected to gearhost DB...");
   }
 });
 
-//w3schools db connect
-app.get('/usertable', function(req, res){
-	  var sql = "CREATE TABLE users (Id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))";
-	  db.query(sql, function (err, result) {
-  		if (err) throw err;
-  		console.log("Result for user table: " + result);
-	  });
-	  res.render('index.jade', {root: VIEWS});
-});
-
 // SQL create table Example
-app.get('/createitems', function(req, result) {
+app.get('/createitems', function(req, res) {
   let sql = 'CREATE TABLE items ( Id int, Name varchar(255), Price varchar(255));';
-  let query = db.query(sql, (err, result) => {
+  let query = db.query(sql, (err, res) => {
     if(err) throw err;
-    console.log("REsult for items table: " + result);
+    console.log("cant create item table", res);
   });
-  //result.send("Well done Emer, items table created...");
-  result.render('index.jade', {root: VIEWS});
+  res.send("Well done Emer, items table created...");
 });
 
 
@@ -53,10 +40,11 @@ app.get('/createitems', function(req, result) {
 app.get('/createproducts', function(req, res) {
   //let sql = 'DROP TABLE products;';
 
-  let sql = 'CREATE TABLE products ( Id int NOT NULL AUTO_INCREMENT PRIMARY KEY, Name varchar(255), Price int, Image varchar(255), Activity varchar(255));'
+  let sql = 'CREATE TABLE products ( Id int NOT NULL AUTO_INCREMENT PRIMARY KEY, Name varchar(255), Price int, Image varchar(255),Activity varchar(255));'
+    console.log();
     let query = db.query(sql, (err, res) => {
       if(err) throw err;
-       console.log(res);
+        console.log("cant create product table", res);
       });
     res.send("Well done products table created...");
 });
@@ -69,23 +57,18 @@ app.get('/insertproduct', function(req, res) {
       
       
     });
-  //res.send("Product Created and inserted to table...");
-  res.render('products.jade', {root: VIEWS}); // use the render command so that the response object renders a HHTML page
-  console.log("Now you are on the products page!");
-  
+  res.send("Product Created and inserted to table...");
 });
 
 
 
-app.post('/new-product', function(req, res) {
-  
-  let sql = 'INSERT INTO products ( Name, Price, Image, Activity) VALUES ("'+req.body.name+'", "'+req.body.price+'", "'+req.body.image+'", "'+req.body.activity+'")'; 
+app.get('/makeme', function(req, res) {
+  let sql = 'INSERT INTO items ( Id, Name, Price) VALUES (1, "Emer", "1000");'; 
   let query = db.query(sql, (err, res) => {
     if(err) throw err;
     console.log(res);
   });
   res.send("Well done Emer, item created...");
-  
 });
 
 app.get('/shoeme', function(req, res) {
@@ -101,7 +84,7 @@ app.get('/sproducts', function(req, res){
  let sql = 'SELECT * FROM products'
   let query = db.query(sql, (err, res1) => {
     if(err.fatal)
-            db.connect();
+            db.connect;
 
     res.render('products.jade', {root: VIEWS,res1});
     console.log(res1);
@@ -126,12 +109,6 @@ app.get('/products', function(req, res){
  // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
   res.render('products.jade', {root: VIEWS}); // use the render command so that the response object renders a HHTML page
   console.log("Now you are on the products page!");
-});
-
-// function to render the products page
-app.get('/new-product', function(req, res){
-  res.render('new-product.jade', {root: VIEWS}); // use the render command so that the response object renders a HHTML page
-  console.log("Now you are on the new product page!");
 });
 
 
